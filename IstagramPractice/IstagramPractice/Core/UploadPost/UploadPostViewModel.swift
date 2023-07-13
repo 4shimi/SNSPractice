a413f28f19cd5196fccbec755aceac10a2680434
Caesar <king81597@gmail.com>
2023-07-14 00:03:21 +0900

    firebase added

---
+++ IstagramPractice/IstagramPractice/Core/UploadPost/UploadPostViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  UploadPostViewModel.swift
+//  IstagramPractice
+//
+//  Created by 정회승 on 2023/07/13.
+//
+
+import SwiftUI
+import PhotosUI
+
+
+@MainActor
+class UploadPostViewModel: ObservableObject {
+    
+    @Published var selectedImage: PhotosPickerItem? {
+        ///didSet?이 뭐임 ->직후 호출
+        didSet { Task
+            {
+                await loadImage(fromItem: selectedImage)
+            }
+        }
+    }
+    @Published var postImage: Image?
+    
+    func loadImage(fromItem item: PhotosPickerItem?) async {
+        guard let item = item else { return }
+        
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: data) else { return }
+        self.postImage = Image(uiImage: uiImage)
+    }
+    
+}
